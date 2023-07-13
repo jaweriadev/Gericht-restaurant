406718402778ee16c0d1e8987e8b81cc804c1a5b
jaweriadev <jaweriaabdulkhaliq@gmail.com>
2023-07-13 13:24:48 +0500

    final about us

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -18,7 +18,9 @@ const Header = () => {
           Explore Menu
         </button>
       </div>
-      <div className="app__wrapper_img"></div>
+      <div className="app__wrapper_img">
+        <img src={images.welcome} alt="header-img" />
+      </div>
     </div>
   );
 };
